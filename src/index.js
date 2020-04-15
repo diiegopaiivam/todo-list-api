1690a6edd67fe6e6d61b48dbcd885916ce3058b5
Diego Paiva <diego.paiva@imobsoft.com.br>
2020-04-14 22:03:28 -0300

    subindo aplicação

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
-var porta = process.env.PORT || 3333
+const porta = process.env.PORT || 3333
 
 const app = express();
 mongoose.connect(process.env.MONGO_URL, {
