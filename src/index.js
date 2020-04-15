7f0b3be3361c61c42c5a1ef6481d275d7b3f7602
Diego Paiva <diego.paiva@imobsoft.com.br>
2020-04-14 21:21:27 -0300

    configurando dotenv

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
+
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
-var porta = process.env.PORT || 3333
+var porta = process.env.PORT || 8080
 
 const app = express();
-mongoose.connect('mongodb+srv://diegopaiva:lucineide42@cluster0-vc3sa.mongodb.net/todo?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false, 
